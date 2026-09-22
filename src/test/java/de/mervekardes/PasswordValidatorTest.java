3e0feaadcdb472489c98d1ba0b89b2d3971f194d
Merve Koksal <mervekoksalcloud@gmail.com>
2026-09-22 12:05:12 +0200

    Add failing tests for digit validation

--- src/test/java/de/mervekardes/PasswordValidatorTest.java
+++ src/test/java/de/mervekardes/PasswordValidatorTest.java
@@ -23,4 +23,34 @@ class PasswordValidatorTest {
     void shouldRejectNullPassword() {
         assertFalse(PasswordValidator.hasMinLength(null, 8));
     }
+
+    @Test
+    void shouldReturnFalseWhenPasswordContainsNoDigit() {
+        assertFalse(PasswordValidator.containsDigit("Abcdefgh"));
+    }
+
+    @Test
+    void shouldReturnTrueWhenPasswordContainsOneDigit() {
+        assertTrue(PasswordValidator.containsDigit("Abcdefg1"));
+    }
+
+    @Test
+    void shouldReturnTrueWhenPasswordContainsMultipleDigits() {
+        assertTrue(PasswordValidator.containsDigit("Ab1c2def"));
+    }
+
+    @Test
+    void shouldReturnTrueWhenPasswordContainsOnlyDigits() {
+        assertTrue(PasswordValidator.containsDigit("12345678"));
+    }
+
+    @Test
+    void shouldIgnoreNonAsciiDigits() {
+        assertFalse(PasswordValidator.containsDigit("Abcdefg\uFF11"));
+    }
+
+    @Test
+    void shouldReturnFalseForNullWhenCheckingDigit() {
+        assertFalse(PasswordValidator.containsDigit(null));
+    }
 }
